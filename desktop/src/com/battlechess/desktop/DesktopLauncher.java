b3398fc038013e52ce51356203ee4f4e7a230ef1
Michael R <mpcooleo@gmail.com>
2018-04-05 00:01:42 -0500

    Initial Commit

---
+++ desktop/src/com/battlechess/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.battlechess.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.battlechess.BattleChess;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BattleChess(), config);
+	}
+}
