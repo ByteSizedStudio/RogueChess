89ea30e52f9a7ccc8b70499f6d20d74442a8e88d
michaelross <mpcooleo@gmail.com>
2018-04-07 19:42:39 -0500

    Fixed Squared

--- core/src/com/battlechess/BattleChess.java
+++ core/src/com/battlechess/BattleChess.java
@@ -47,7 +47,7 @@ public class BattleChess extends Game {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		if(set)
-			GameState.getInstance()
+			GameState.getInstance().getScreen().render(Gdx.graphics.getDeltaTime());
 		batch.begin();
 		//batch.draw(img, 0, 0);
 		batch.end();
