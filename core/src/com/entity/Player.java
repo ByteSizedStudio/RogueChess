e054fe592ef930568b96ef89c4b66fc17c83b233
Quincy <quincy.johnson09@k12.leanderisd.org>
2018-04-10 18:55:14 -0500

    Divided board into 5x5 sections instead of 3x3.

--- core/src/com/entity/Player.java
+++ core/src/com/entity/Player.java
@@ -105,7 +105,7 @@ public class Player extends Interactables{
 
 
 		//Delay is normally 900. small for testing
-		if(TimeUtils.timeSinceMillis(inputDelay) > 600L && !spaceBar &&!isAttacking) {
+		if(TimeUtils.timeSinceMillis(inputDelay) > 300L && !spaceBar &&!isAttacking) {
 
 			if (Gdx.input.isKeyPressed(Input.Keys.W) && isValidMove(yPos + 1, xPos)) {
 				yPos++;
